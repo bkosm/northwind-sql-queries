ffb5c27f3b3076c8db312b769edfa1e035e98873
Bartosz Kosmala <45885942+bart-kosmala@users.noreply.github.com>
2020-03-20 21:47:02 +0100

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -42,3 +42,44 @@ ON p.ProductID = od.ProductID
 FULL OUTER JOIN Categories c
 ON c.CategoryID = p.CategoryID
 GROUP BY c.CategoryName;
+-- 8
+UPDATE Products
+SET UnitPrice = UnitPrice * 1.1
+WHERE EXISTS (SELECT *
+FROM Orders o
+INNER JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+WHERE od.ProductID = ProductID
+GROUP BY od.ProductID
+HAVING COUNT(DISTINCT o.CustomerID) > 10);
+-- 9
+CREATE TABLE "Customer Product Employee" (
+	CompanyName	nvarchar(40) not null,
+	ProductName	nvarchar(40) not null,
+	FullName	nvarchar(31) not null
+);
+INSERT INTO "Customer Product Employee"
+SELECT c.CompanyName, p.ProductName, e.FirstName + ' ' + e.LastName
+FROM Orders o
+INNER JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+INNER JOIN Customers c
+ON c.CustomerID = o.CustomerID
+INNER JOIN Employees e
+ON e.EmployeeID = o.EmployeeID
+INNER JOIN Products p
+ON p.ProductID = od.ProductID;
+-- 10
+INSERT INTO Customers
+SELECT CAST(s.SupplierID AS nchar(5)), s.CompanyName, s.ContactName, s.ContactTitle, s.Address, s.City, s.Region, s.PostalCode, s.Country, s.Phone, s.Fax
+FROM Suppliers s
+LEFT JOIN Products p
+ON p.SupplierID = s.SupplierID
+WHERE p.SupplierID IS NULL;
+-- 11
+CREATE VIEW "Orders Prices" AS
+SELECT o.OrderID, SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) Total
+FROM Orders o
+INNER JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+GROUP BY o.OrderID;
