50d86d85b95e9717d294eeb34a490fd5abcb8c41
Bartosz Kosmala <45885942+bart-kosmala@users.noreply.github.com>
2020-04-07 18:04:20 +0600

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -5,9 +5,10 @@ SELECT * FROM Orders
 WHERE OrderDate > @Od AND OrderDate < @Do
 ORDER BY OrderDate ASC
 GO
+
 EXEC zamowieniaMiedzy '1996-10-10', '2000-01-01'
 -- 2
-CREATE PROCEDURE zamowieniaZ @fraza NVARCHAR(15), @cena MONEY
+CREATE PROCEDURE zamowieniaZ @Fraza NVARCHAR(15), @Cena MONEY
 AS
 SELECT *
 FROM Orders o
@@ -18,6 +19,7 @@ ON o.OrderID = od.OrderID
 GROUP BY o.OrderID
 HAVING SUM(od.UnitPrice*od.Quantity*(1-od.Discount)) > @Cena)
 GO
+
 EXEC zamowieniaZ 'USA', 10000
 -- 3
 CREATE TRIGGER wyswietl
@@ -35,3 +37,85 @@ PRINT 'P:' + @produkt + '; K: ' + CAST(@kategoria AS VARCHAR(40)) + ';'
 END
 INSERT INTO Products(ProductName, CategoryID)
 VALUES ('Produkt', 1)
+-- 4
+CREATE PROCEDURE ustawRabat @Cena MONEY, @Rabat REAL
+AS
+UPDATE [Order Details]
+SET Discount = @Rabat
+WHERE UnitPrice * Quantity > @Cena
+GO
+
+EXEC ustawRabat 100, 0.2
+-- 5
+ALTER TABLE Orders
+ADD LastModified DATETIME;
+
+CREATE TRIGGER odswiezModyfikacje
+ON Orders
+AFTER INSERT, UPDATE, DELETE
+AS
+DECLARE @TempKey int
+IF EXISTS (SELECT * FROM inserted I) AND EXISTS (SELECT * FROM deleted)
+BEGIN
+  SELECT @TempKey = I.OrderID FROM inserted I
+	UPDATE Orders
+	SET	LastModified = GETDATE()
+	WHERE OrderID = @TempKey
+  SELECT @TempKey = D.OrderID FROM deleted D
+	UPDATE Orders
+	SET	LastModified = GETDATE()
+	WHERE OrderID = @TempKey
+END
+IF EXISTS (SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted)
+BEGIN
+  SELECT @TempKey = I.OrderID FROM inserted I
+	UPDATE Orders
+	SET	LastModified = GETDATE()
+	WHERE OrderID = @TempKey
+END
+IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted)
+BEGIN
+	SELECT @TempKey = D.OrderID FROM deleted D
+	UPDATE Orders
+	SET	LastModified = GETDATE()
+	WHERE OrderID = @TempKey
+END
+
+UPDATE Orders 
+SET Freight = 12
+WHERE OrderID = 10248;
+-- 6
+CREATE TRIGGER pokazProdukty
+ON Customers
+AFTER INSERT
+AS
+DECLARE @TempCity NVARCHAR(15), @TempProduct NVARCHAR(40)
+BEGIN
+	SELECT @TempCity = I.City FROM inserted I
+
+	DECLARE cur CURSOR FOR
+	SELECT P.ProductName FROM Customers C
+	INNER JOIN Orders O
+	ON O.CustomerID = C.CustomerID
+	INNER JOIN [Order Details] OD
+	ON OD.OrderID = O.OrderID
+	INNER JOIN Products P
+	ON OD.ProductID = P.ProductID
+	WHERE C.City LIKE @TempCity
+
+	OPEN cur
+
+	FETCH NEXT FROM cur INTO @TempProduct;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN   
+		PRINT 'Produkt kupiony w ' + @TempCity + ': ' + @TempProduct
+		FETCH NEXT FROM cur INTO @TempProduct;
+	END
+
+	CLOSE cur;
+	DEALLOCATE cur;
+END
+
+INSERT INTO Customers (CustomerID, CompanyName, City)
+VALUES ('90837', 'NowySklep', 'Graz')
+-- 7
