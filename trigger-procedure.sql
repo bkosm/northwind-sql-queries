9561e342aab13b7aeb6153938dfcd95fb6775231
Bartosz Kosmala <45885942+bart-kosmala@users.noreply.github.com>
2020-04-07 20:39:51 +0600

    Update and rename lab4.sql to trigger-procedure.sql

--- trigger-procedure.sql
+++ trigger-procedure.sql
@@ -1,4 +1,4 @@
--- 1
+-- 1 Skonstruuj procedurę wyświetlającą informacje o zamówieniach dokonanych pomiędzy podanymi datami.
 CREATE PROCEDURE zamowieniaMiedzy @Od DATETIME, @Do DATETIME
 AS
 SELECT * FROM Orders
@@ -7,37 +7,38 @@ ORDER BY OrderDate ASC
 GO
 
 EXEC zamowieniaMiedzy '1996-10-10', '2000-01-01'
--- 2
+
+-- 2 Skonstruuj procedurę wyświetlającą informacje o zamówieniach dostarczanych do kraju, którego nazwa zawiera podaną frazę oraz o wartości większej niż (również) podana.
 CREATE PROCEDURE zamowieniaZ @Fraza NVARCHAR(15), @Cena MONEY
 AS
 SELECT *
 FROM Orders o
 WHERE o.ShipCountry LIKE '%'+@Fraza+'%' AND EXISTS (SELECT o.OrderId
-FROM [Order Details] od
-INNER JOIN Orders o 
-ON o.OrderID = od.OrderID
-GROUP BY o.OrderID
-HAVING SUM(od.UnitPrice*od.Quantity*(1-od.Discount)) > @Cena)
+													FROM [Order Details] od
+													INNER JOIN Orders o 
+													ON o.OrderID = od.OrderID
+													GROUP BY o.OrderID
+													HAVING SUM(od.UnitPrice*od.Quantity*(1-od.Discount)) > @Cena)
 GO
 
 EXEC zamowieniaZ 'USA', 10000
--- 3
+
+-- 3 Zaprojektuj wyzwalacz wypisujący (PRINT) nazwę produktu oraz nazwę kategorii po każdorazowym dodaniu nowego produktu do tabeli ‘Products’
 CREATE TRIGGER wyswietl
 ON Products
 AFTER INSERT
-AS DECLARE
-@produkt NVARCHAR(40),
-@kategoria INT;
-
+AS DECLARE @produkt NVARCHAR(40), @kategoria INT
 BEGIN
 SELECT @produkt = ProductName FROM INSERTED
 SELECT @kategoria = CategoryID FROM INSERTED
 
 PRINT 'P:' + @produkt + '; K: ' + CAST(@kategoria AS VARCHAR(40)) + ';'
 END
+
 INSERT INTO Products(ProductName, CategoryID)
 VALUES ('Produkt', 1)
--- 4
+
+-- 4 Skonstruuj procedurę ustawiającą rabat (Discount) o podanej wysokości produktom, których wartość (cena jednostkowa · ilość) jest większa niż podana wartość.
 CREATE PROCEDURE ustawRabat @Cena MONEY, @Rabat REAL
 AS
 UPDATE [Order Details]
@@ -46,7 +47,8 @@ WHERE UnitPrice * Quantity > @Cena
 GO
 
 EXEC ustawRabat 100, 0.2
--- 5
+
+-- 5 Dodaj nową kolumnę do tabeli ‘Orders’ o nazwie ‘LastModified’ przechowującą datę ostatniej modyfikacji zamówienia. Następnie zaprojektuj wyzwalacz uaktualniający datę modyfikacji zamówienia w odpowiedzi na jakąkolwiek zmianę (INSERT, UPDATE, DELETE) w tabeli ‘Order Details’ (dotyczącą danego zamówienia).
 ALTER TABLE Orders
 ADD LastModified DATETIME;
 
@@ -57,18 +59,19 @@ AS
 DECLARE @TempKey int
 IF EXISTS (SELECT * FROM inserted I) AND EXISTS (SELECT * FROM deleted)
 BEGIN
-  SELECT @TempKey = I.OrderID FROM inserted I
+  	SELECT @TempKey = I.OrderID FROM inserted I
 	UPDATE Orders
 	SET	LastModified = GETDATE()
 	WHERE OrderID = @TempKey
-  SELECT @TempKey = D.OrderID FROM deleted D
+	
+  	SELECT @TempKey = D.OrderID FROM deleted D
 	UPDATE Orders
 	SET	LastModified = GETDATE()
 	WHERE OrderID = @TempKey
 END
 IF EXISTS (SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted)
 BEGIN
-  SELECT @TempKey = I.OrderID FROM inserted I
+  	SELECT @TempKey = I.OrderID FROM inserted I
 	UPDATE Orders
 	SET	LastModified = GETDATE()
 	WHERE OrderID = @TempKey
@@ -84,12 +87,12 @@ END
 UPDATE Orders 
 SET Freight = 12
 WHERE OrderID = 10248;
--- 6
+
+-- 6 Zaprojektuj wyzwalacz, który po dodaniu nowego klienta wypisze (PRINT) informacje o produktach zakupionych przez klientów z tego samego miasta co nowy klient.
 CREATE TRIGGER pokazProdukty
 ON Customers
 AFTER INSERT
-AS
-DECLARE @TempCity NVARCHAR(15), @TempProduct NVARCHAR(40)
+AS DECLARE @TempCity NVARCHAR(15), @TempProduct NVARCHAR(40)
 BEGIN
 	SELECT @TempCity = I.City FROM inserted I
 
@@ -118,7 +121,8 @@ END
 
 INSERT INTO Customers (CustomerID, CompanyName, City)
 VALUES ('90837', 'NowySklep', 'Graz')
--- 7
+
+-- 7 Zaprojektuj wyzwalacz, który przed dodaniem nowego produktu do zamówienia sprawdzi czy liczba zamawianych sztuk nie przekracza bieżącego stanu magazynowego (UnitsInStock). Jeżeli w magazynie nie ma wystarczającej liczby sztuk zamawianego produktu transakcję należy wycofać, w przeciwnym przypadku należy uaktualnić stan magazynowy oraz dodać produkt do zamówienia 
 CREATE TRIGGER sprawdzStan
 ON [Order Details]
 INSTEAD OF INSERT
@@ -148,7 +152,8 @@ END
 
 INSERT INTO [Order Details]
 VALUES (10248, 5, 31, 2, 0)
--- 8
+
+-- 8 Skonstruuj procedurę dodającą produkt (o podanej nazwie) do zamówienia klienta (o podanej nazwie firmy). Jeżeli dany klient nie posiada żadnych zamówień, to utwórz nowe przypisując do niego pracownika, który „obsługuje” najmniej zamówień. W przypadku gdy klient posiada zamówienia, to dodaj produkt do tego, które zawiera najmniej produktów. 
 CREATE PROCEDURE dodajProduktDo @NazwaFirmy NVARCHAR(40), @NazwaProduktu NVARCHAR(40)
 AS
 DECLARE @CustomerId NCHAR(5)
@@ -169,10 +174,10 @@ BEGIN
 	DECLARE @LastOrderId INT
 
 	SELECT @EmployeeId = Out.EmployeeID
-	FROM (SELECT TOP 1 EmployeeID, COUNT(OrderID) Amount
-	FROM Orders
-	GROUP BY EmployeeID
-	ORDER BY Amount ASC) Out
+	FROM (	SELECT TOP 1 EmployeeID, COUNT(OrderID) Amount
+			FROM Orders
+			GROUP BY EmployeeID
+			ORDER BY Amount ASC) Out
 
 	SELECT @LastOrderId = MAX(OrderID) 
 	FROM Orders
@@ -189,15 +194,17 @@ END
 DECLARE @OrderId INT
 
 SELECT @OrderId = Out.OrderID
-FROM (SELECT TOP 1 OD.OrderID, COUNT(OD.ProductID) Minimal
-FROM Orders O
-INNER JOIN [Order Details] OD
-ON O.OrderID = OD.OrderID
-WHERE O.CustomerID = @CustomerId
-GROUP BY OD.OrderID
-ORDER BY Minimal ASC) Out
+FROM (	SELECT TOP 1 OD.OrderID, COUNT(OD.ProductID) Minimal
+		FROM Orders O
+		INNER JOIN [Order Details] OD
+		ON O.OrderID = OD.OrderID
+		WHERE O.CustomerID = @CustomerId
+		GROUP BY OD.OrderID
+		ORDER BY Minimal ASC) Out
 
 INSERT INTO [Order Details] 
 VALUES (@OrderId, @ProductId, @UnitPrice, 1, 0)
 
 GO
+
+EXEC dodajProduktDo 'Lonesome Pine Restaurant', 'Chang'
